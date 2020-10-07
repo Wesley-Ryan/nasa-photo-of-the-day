437bbf4dcb9f168745219bc66c9df47bf99fdc08
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-10-07 12:02:14 -0700

    Added axios, returning data from Nasa API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import Axios from "axios";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    axios
+      .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log("There was an error fetching the data:", err);
+      });
+  }, []);
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        app! Have fun{" "}
+        <span role="img" aria-label="go!">
+          🚀
+        </span>
+        !
       </p>
     </div>
   );
